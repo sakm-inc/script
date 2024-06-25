779117ca36304e2510d06f96b2484af81bfd524c
sakm-inc <toghrul.rzayev1@gmail.com>
2024-06-26 01:08:26 +0300

    final

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1092,22 +1092,31 @@ fecho ""
 		echo "Client $CLIENT added."
 	fi
 
-	# Home directory of the user, where the client configuration will be written
-	if [ -e "/home/${CLIENT}" ]; then
-		# if $1 is a user name
-		homeDir="/home/${CLIENT}"
-	elif [ "${SUDO_USER}" ]; then
-		# if not, use SUDO_USER
-		if [ "${SUDO_USER}" == "root" ]; then
-			# If running sudo as root
-			homeDir="/root"
-		else
-			homeDir="/home/${SUDO_USER}"
-		fi
-	else
-		# if not SUDO_USER, use /root
-		homeDir="/root"
-	fi
+# Define the dedicated folder
+dedicatedFolder="/public"
+
+# Home directory of the user, where the client configuration will be written
+if [ -e "/home/${CLIENT}" ]; then
+    # if $1 is a user name
+    homeDir="/home/${CLIENT}"
+elif [ "${SUDO_USER}" ]; then
+    # if not, use SUDO_USER
+    if [ "${SUDO_USER}" == "root" ]; then
+        # If running sudo as root
+        homeDir="$dedicatedFolder"
+    else
+        homeDir="/home/${SUDO_USER}"
+    fi
+else
+    # if not SUDO_USER, use /root
+    homeDir="$dedicatedFolder"
+fi
+
+# Ensure the dedicated folder exists
+mkdir -p "$homeDir"
+
+# Now you can save the client configuration file in the homeDir
+echo "Client configuration will be written to $homeDir"
 
 	# Determine if we use tls-auth or tls-crypt
 	if grep -qs "^tls-crypt" /etc/openvpn/server.conf; then
